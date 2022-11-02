432b38cad24e97a0170b07d220ee277886e32323
dramforever <dramforever@live.com>
2022-11-02 23:14:09 +0800

    test_helpers: Add missing include

--- src/test_helpers.h
+++ src/test_helpers.h
@@ -1,6 +1,8 @@
 #ifndef _TEST_HELPERS_H
 #define _TEST_HELPERS_H
 
+#include "printf.h"
+
 #define LOG(msg, ...) printf("%s: " msg "\n", __func__, ##__VA_ARGS__)
 
 #define ASSERT(check, msg, ...)                                      \
