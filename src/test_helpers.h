dcb6727257a6e634ee60d9cc3ad08001e3272e71
dramforever <dramforever@live.com>
2022-11-02 21:24:42 +0800

    Add test helper macros

---
+++ src/test_helpers.h
@@ -0,0 +1,16 @@
+#ifndef _TEST_HELPERS_H
+#define _TEST_HELPERS_H
+
+#define LOG(msg, ...) printf("%s: " msg "\n", __func__, ##__VA_ARGS__)
+
+#define ASSERT(check, msg, ...)                                      \
+	do {                                                         \
+		if (check)                                           \
+			printf("[  OK  ] " msg "\n", ##__VA_ARGS__); \
+		else                                                 \
+			printf("[ FAIL ] " msg "\n", ##__VA_ARGS__); \
+	} while (0)
+
+#define STACK(buf) ((void *)((char *)buf + sizeof(buf)))
+
+#endif
